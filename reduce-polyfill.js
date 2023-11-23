c4417d733a5a8e81cfa5ac53264ea0c25a6ddbe1
Shailender Pandey <32347434+shailender999@users.noreply.github.com>
2023-11-23 14:57:06 +0530

    Update reduce-polyfill.js

--- reduce-polyfill.js
+++ reduce-polyfill.js
@@ -1,10 +1,15 @@
 // Write a polyfill for "reduce" function in Javascript 
 
 function arrayReduce(callback, initialValue) {
+    // if callback is not a function, then throw type error
     if(typeof callback !== 'function'){
         throw new TypeError('Callback must be a function');
     }
     let _arr = this;
+    // if array length is 0 and initialValue is also not defined, then throw type error
+    if(_arr.length === 0 && (initialValue === undefined)) {
+        throw new TypeError('Reduce of empty array with no initial Value');
+    }
     let output = initialValue;
     for(let i=0; i< _arr.length; i++) {
         if(output !== undefined) {
@@ -13,12 +18,14 @@ function arrayReduce(callback, initialValue) {
             output = _arr[i];
         }
     }
+    // return reduced output.
+    // if array is empty, return initialValue if available else undefined
     return output;
 }
 Array.prototype.arrayReduce = arrayReduce;
 
 // Testing the pollyfill with static array
-const arr = [2,3,4,5]
+const arr = [2,3,4,5,6]
 
 // With native reduce method
 console.log(arr.reduce((sum, current) => sum + current, 0));
